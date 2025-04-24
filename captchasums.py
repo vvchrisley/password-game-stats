4f240fe870c414236669a9ddd7c51a300850e11c
SyntacticSweetTooth <twilight2095@proton.me>
2025-04-23 22:11:20 -0400

    making sum_of_digits a function makes the code a lot prettier
    i really need to remember that functions often have that effect

--- captchasums.py
+++ captchasums.py
@@ -1,22 +1,20 @@
 import re
 import pandas as pd
 
-# track how many captchas have what sums
-sum_counts = pd.DataFrame({'count': [0] * 26}, index=list(range(25))+['>=25'])
-
 # get captchas from file
-captcha_list = open('input/captchas', 'r').read().split(',')
+captcha_list = open('captcha_stuff/captchas', 'r').read().split(',')
 
-for captcha in captcha_list:
-    # sum digits from captcha
-    digits = re.findall(r'\d', captcha)
-    sum_of_digits = sum(int(digit) for digit in digits)
-    # increment the appropriate sum count
-    if sum_of_digits < 25:
-        sum_counts['count'][sum_of_digits] += 1
-    else:
-        sum_counts['count']['>=25'] += 1
+def sum_of_digits(string):
+    """Return the sum of all digits in a string. 
+    >>> sum_of_digits("2b827")
+    19
+    """
+    digits = re.findall(r'\d', string)
+    return sum(int(digit) for digit in digits)
+
+# get the sum of digits in each captcha
+sums = [sum_of_digits(captcha) for captcha in captcha_list]
 
 # print results
-print("Total CAPTCHAs:", sum(sum_counts['count']), "\n")
-print(sum_counts)
+print("# of CAPTCHAs:", len(captcha_list))
+print(pd.value_counts(sums).sort_index())
