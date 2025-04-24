a5c554b057a1ca1fba63d6237b8ed1d3e5e973b9
SyntacticSweetTooth <twilight2095@proton.me>
2025-04-23 20:17:09 -0400

    figured out how to use pandas efficiently

--- captchasums.py
+++ captchasums.py
@@ -1,12 +1,11 @@
 import re
+import pandas as pd
 
 # track how many captchas have what sums
-sum_counts = [0] * 26
+sum_counts = pd.DataFrame({'count': [0] * 26}, index=list(range(25))+['>=25'])
 
 # get captchas from file
 captcha_list = open('input/captchas', 'r').read().split(',')
-# remove quotation marks
-captcha_list = [captcha.strip('"') for captcha in captcha_list]
 
 for captcha in captcha_list:
     # sum digits from captcha
@@ -14,13 +13,10 @@ for captcha in captcha_list:
     sum_of_digits = sum(int(digit) for digit in digits)
     # increment the appropriate sum count
     if sum_of_digits < 25:
-        sum_counts[sum_of_digits] += 1
+        sum_counts['count'][sum_of_digits] += 1
     else:
-        sum_counts[25] += 1
+        sum_counts['count']['>=25'] += 1
 
 # print results
-print("Total CAPTCHAs:", sum(sum_counts), "\n")
-print("Sum:\t# of CAPTCHAs:")
-for i, count in enumerate(sum_counts[0:25]):
-    print(i, "\t", count)
-print(">=25\t", sum_counts[25])
+print("Total CAPTCHAs:", sum(sum_counts['count']), "\n")
+print(sum_counts)
