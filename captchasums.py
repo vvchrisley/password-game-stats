8270081b2e8eaa02a3795dee47a86e73b73e264d
SyntacticSweetTooth <twilight2095@proton.me>
2025-05-01 23:49:57 -0400

    put code in main function
    so i can import the sum_of_digits function without running everything

--- captchasums.py
+++ captchasums.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-# get captchas from file
-captcha_list = open('input/captchas', 'r').read().split(',')
-
 def sum_of_digits(string):
     """Return the sum of all digits in a string. 
     >>> sum_of_digits("2b827")
@@ -10,10 +7,17 @@ def sum_of_digits(string):
     """
     return sum(int(char) for char in string if char.isdigit())
 
-# get the sum of digits in each captcha
-sums = pd.Series([sum_of_digits(captcha) for captcha in captcha_list])
+def main():
+    # get captchas from file
+    captcha_list = open('input/captchas', 'r').read().split(',')
 
-# print results
-print("Total CAPTCHAs:", len(captcha_list), "\n")
-print("sum  # of CAPTCHAs")
-print(sums.value_counts().sort_index())
+    # get the sum of digits in each captcha
+    sums = pd.Series([sum_of_digits(captcha) for captcha in captcha_list])
+
+    # print results
+    print("Total CAPTCHAs:", len(captcha_list), "\n")
+    print("sum  # of CAPTCHAs")
+    print(sums.value_counts().sort_index())
+
+if __name__ == '__main__':
+    main()
