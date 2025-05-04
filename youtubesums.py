6234f049d8719d82761d5e8520f82b6840b5368c
SyntacticSweetTooth <twilight2095@proton.me>
2025-05-04 01:12:00 -0400

    use plot_frequency

--- youtubesums.py
+++ youtubesums.py
@@ -1,24 +1,16 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
-from captchasums import sum_of_digits
+import pandas as pd
+from captchasums import sums_of_digits, plot_frequency
 
 def main():
-    # set plot labels
-    plt.title("Sum of digits in YouTube video IDs")
-    plt.xlabel("Sum")
-    plt.ylabel("Frequency")
-
     # get video ids from file
     video_ids = pd.read_csv('input/youtube.csv', usecols=['video_id']).squeeze()
 
     # get the sum of digits in each video id
-    sums = pd.Series([sum_of_digits(vid_id) for vid_id in video_ids])
-    sum_counts = sums.value_counts().sort_index()
+    sums = sums_of_digits(video_ids)
     print(sums.describe())
 
     # plot results
-    plt.bar(sum_counts.index, sum_counts)
-    plt.show()
+    plot_frequency(sums, 'Sum', 'Sum of digits in YouTube video IDs')
 
 if __name__ == '__main__':
     main()
