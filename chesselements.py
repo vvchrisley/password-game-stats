d6cfe5de3e1073cf182e209763d08122ebfedc51
SyntacticSweetTooth <twilight2095@proton.me>
2025-05-04 00:03:59 -0400

    add main function
    and a bunch of small changes. a lot of this file can probably be written better, but i will deal with that another day.

--- chesselements.py
+++ chesselements.py
@@ -2,28 +2,8 @@ import re
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# set plot labels
-plt.title("Frequency of elements in chess moves")
-plt.xlabel("Atomic number")
-plt.ylabel("Frequency")
-
-# get moves from chess file
-chess = open('input/chess', 'r')
-move_pattern = re.compile(r'[RNBKQ]?x?[a-h][1-8]\+?')
-moves = [re.search(move_pattern, line).group() for line in chess if 'sol:' in line]
-chess.close()
-
-# get info from elements file
-elements = pd.read_csv('input/elements.csv').set_index('Symbol')
-
-# get elements that could show up in chess moves
-element_pattern = re.compile(r'^[RNBKQ][a-hx]?$')
-sneaky_elements = [ele for ele in elements.index if re.search(element_pattern, ele)]
-# sort so two-letter elements are first
-sneaky_elements = sorted(sneaky_elements, key=len, reverse=True)
-
 def first_matching_term(string, terms):
-    """Return the first list item appearing in the string.
+    """Return the first list item appearing in the string, or None if none.
     >>> first_matching_term('Ne', ['Rg', 'Ne', 'N'])
     'Ne'"""
     for term in terms:
@@ -31,26 +11,44 @@ def first_matching_term(string, terms):
             return term
     return None
 
-def get_atomic_number(element):
-    """Return the atomic number of an element, or 0 if None."""
-    try:
-        return elements.loc[element, 'Atomic Number']
-    except KeyError:
-        return 0
+def main():
+    # get moves from chess file
+    chess = open('input/chess', 'r')
+    move_pattern = re.compile(r'[RNBKQ]?x?[a-h][1-8]\+?')
+    moves = [re.search(move_pattern, line).group() for line in chess if 'sol:' in line]
+    chess.close()
 
-# list the element if any for each move
-elements_in_moves = [first_matching_term(move, sneaky_elements) for move in moves]
-# and the corresponding atomic number
-move_data = pd.DataFrame({'Element': elements_in_moves,
-'Atomic Number': [get_atomic_number(ele) for ele in elements_in_moves]},
-index=moves)
+    # get info from elements file
+    elements = pd.read_csv('input/elements.csv').set_index('Symbol')
 
-# print results
-print("Total moves:", len(moves))
-print("Moves ending in check:", sum(['+' in move for move in moves]))
-print("Moves containing an element:", sum([bool(ele) for ele in move_data['Element']]), "\n")
-print(move_data.value_counts())
+    # get elements that could show up in chess moves
+    element_pattern = re.compile(r'^[RNBKQ][a-hx]?$')
+    sneaky_elements = [ele for ele in elements.index if re.search(element_pattern, ele)]
+    # sort so two-letter elements are first
+    sneaky_elements = sorted(sneaky_elements, key=len, reverse=True)
 
-# plot results
-plt.hist(move_data['Atomic Number'], bins=range(118))
-plt.show()
+    # list the element if any for each move
+    elements_in_moves = [first_matching_term(move, sneaky_elements) for move in moves]
+    # and the corresponding atomic number
+    atomic_numbers = [elements.loc[ele, 'Atomic Number'] if ele else 0 
+    for ele in elements_in_moves]
+
+    move_data = pd.DataFrame({'Atomic Number': atomic_numbers,
+    'Element': elements_in_moves})
+
+    # print results
+    print("Total moves:", len(moves))
+    print("Moves ending in check:", sum(['+' in move for move in moves]))
+    print("Moves containing an element:", move_data['Element'].astype(bool).sum(), "\n")
+    print(move_data.value_counts(sort=False))
+
+    """
+    # plot results
+    plt.title("Frequency of elements in chess moves")
+    plt.xlabel("Atomic number")
+    plt.ylabel("Frequency")
+    plt.hist(move_data['Atomic Number'], bins=range(118))
+    plt.show()"""
+
+if __name__ == '__main__':
+    main()
