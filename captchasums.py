007abb127e02d7548f262776144dc624f0a39159
SyntacticSweetTooth <twilight2095@proton.me>
2025-05-04 00:50:58 -0400

    make plot_frequency reusable

--- captchasums.py
+++ captchasums.py
@@ -1,29 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def sum_of_digits(string):
-    """Return the sum of all digits in a string. 
-    >>> sum_of_digits('2b827')
-    19
+def sums_of_digits(str_list):
+    """Return the sum of digits in each string in a list. 
+    >>> sum_of_digits(['2b827', '3bfnd'])
+    0   19
+    1   3
+    Length: 2, dtype: int64
     """
-    return sum(int(char) for char in string if char.isdigit())
+    sum_digits = lambda s: sum(int(char) for char in s if char.isdigit())
+    return pd.Series([sum_digits(string) for string in str_list])
+
+def plot_frequency(series, xlabel='Value', title='Value counts'):
+    """Graph the frequency of items in a Series."""
+    data = series.value_counts()
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel('Frequency')
+    plt.bar(data.index, data)
+    plt.show()
 
 def main():
     # get captchas from file
     captcha_list = open('input/captchas', 'r').read().split(',')
 
-    # get the sum of digits in each captcha
-    sums = pd.Series([sum_of_digits(captcha) for captcha in captcha_list])
-    sum_counts = sums.value_counts().sort_index()
+    # get captcha sums
+    sums = sums_of_digits(captcha_list)
     
     # plot results
-    num_captchas = str(len(captcha_list))
-    title = 'Sum of digits in CAPTCHAs (n = ' + num_captchas + ')'
-    plt.title(title)
-    plt.xlabel('Sum')
-    plt.ylabel('Frequency')
-    plt.bar(sum_counts.index, sum_counts)
-    plt.show()
+    title = 'Sum of digits in CAPTCHAs (n = ' + str(len(captcha_list)) + ')'
+    plot_frequency(sums, 'Sum', title)
 
 if __name__ == '__main__':
     main()
