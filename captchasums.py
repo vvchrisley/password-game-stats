f21636fd3036b76126ec24ee20f68f770558dafd
SyntacticSweetTooth <twilight2095@proton.me>
2025-05-03 20:10:26 -0400

    plot results
    and make all quotes single

--- captchasums.py
+++ captchasums.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def sum_of_digits(string):
     """Return the sum of all digits in a string. 
-    >>> sum_of_digits("2b827")
+    >>> sum_of_digits('2b827')
     19
     """
     return sum(int(char) for char in string if char.isdigit())
@@ -13,11 +14,16 @@ def main():
 
     # get the sum of digits in each captcha
     sums = pd.Series([sum_of_digits(captcha) for captcha in captcha_list])
-
-    # print results
-    print("Total CAPTCHAs:", len(captcha_list), "\n")
-    print("sum  # of CAPTCHAs")
-    print(sums.value_counts().sort_index())
+    sum_counts = sums.value_counts().sort_index()
+    
+    # plot results
+    num_captchas = str(len(captcha_list))
+    title = 'Sum of digits in CAPTCHAs (n = ' + num_captchas + ')'
+    plt.title(title)
+    plt.xlabel('Sum')
+    plt.ylabel('Frequency')
+    plt.bar(sum_counts.index, sum_counts)
+    plt.show()
 
 if __name__ == '__main__':
     main()
